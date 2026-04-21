e55da1245e7f7b46bb7ed533ff5f54603e006a18
hamo80577 <mohamed.mahmoud.412860_ibs.ext@talabat.com>
2026-04-21 15:47:29 +0200

    feat: add auth session token helpers

--- packages/authn/src/index.ts
+++ packages/authn/src/index.ts
@@ -1,2 +1,8 @@
 export const authnPackageName = "@vision/authn" as const;
 export { hashPassword, verifyPassword } from "./password";
+export {
+  createSessionToken,
+  hashSessionSecret,
+  parseSessionToken,
+  verifySessionSecret,
+} from "./session-token";
